95b99061d9960edf4654b599a49e88c47c034fc1
Sudhanshu Shekhar <sudhanshushekhar8210@gmail.com>
2026-09-03 18:43:54 +0530

    add facebook pages and instagram accounts

--- models/MetaConnection.js
+++ models/MetaConnection.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const MetaConnectionSchema = new mongoose.Schema(
   {
-    // Meta / Facebook User ID
     metaUserId: {
       type: String,
       required: true,
@@ -10,31 +9,30 @@ const MetaConnectionSchema = new mongoose.Schema(
       index: true,
     },
 
-    // Encrypted Meta Access Token
     accessToken: {
       type: String,
       required: true,
     },
 
-    // Token expiry
     tokenExpiresAt: {
       type: Date,
       default: null,
     },
 
-    // Connection status
     connected: {
       type: Boolean,
       default: true,
     },
 
-    // Connection date
     connectedAt: {
       type: Date,
       default: Date.now,
     },
 
+    // ---------------------------------------------
     // Meta Ad Accounts
+    // ---------------------------------------------
+
     adAccounts: [
       {
         id: {
@@ -63,11 +61,76 @@ const MetaConnectionSchema = new mongoose.Schema(
         },
       },
     ],
+
+    // ---------------------------------------------
+    // Facebook Pages
+    // ---------------------------------------------
+
+    facebookPages: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+
+        name: {
+          type: String,
+          default: "",
+        },
+
+        access_token: {
+          type: String,
+          default: "",
+        },
+
+        category: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
+
+    // ---------------------------------------------
+    // Instagram Accounts
+    // ---------------------------------------------
+
+    instagramAccounts: [
+      {
+        id: {
+          type: String,
+          required: true,
+        },
+
+        username: {
+          type: String,
+          default: "",
+        },
+
+        name: {
+          type: String,
+          default: "",
+        },
+
+        profile_picture_url: {
+          type: String,
+          default: "",
+        },
+
+        facebookPageId: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
 
-export default mongoose.models.MetaConnection ||
-  mongoose.model("MetaConnection", MetaConnectionSchema);
+export default
+  mongoose.models.MetaConnection ||
+  mongoose.model(
+    "MetaConnection",
+    MetaConnectionSchema
+  );
